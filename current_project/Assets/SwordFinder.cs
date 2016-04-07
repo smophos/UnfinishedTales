09c70c3e1b217f7d71d9abce7505d40c98203237
smophos <saumya.smo@gmail.com>
2016-04-06 21:32:12 -0500

    Adding tutorial section

---
+++ current_project/Assets/SwordFinder.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwordFinder : MonoBehaviour
+{
+
+    DialoguePanel panel;
+    ProgressTracker tracker;
+
+    // Use this for initialization
+    void Start()
+    {
+        panel = DialoguePanel.Instance();
+        tracker = ProgressTracker.GetProgressTracker();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /* void OnTriggerEnter(Collider other)
+     {
+             panel.ShowDialogue("Bridge Ahead!!!");
+     }*/
+
+    void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Space");
+            panel.ShowDialogue("found your sword!!!");
+        }
+    }
+}
